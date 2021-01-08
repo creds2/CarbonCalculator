51d24ea994c3ff5ec8d68191f21415870c58d967
Malcolm Morgan <mem48@users.noreply.github.com>
2021-01-08 03:45:51 +0000

    add lables

--- www/js/map_layerdesc.js
+++ www/js/map_layerdesc.js
@@ -8,6 +8,15 @@ function switchLayerDesc(layer) {
   case "car_emissions_grade":
     document.getElementById("layerdesc").innerHTML = `<p>Estimated avearge carbon footprint per person from driving cars. Emissions account for mileage driven by cars which are registed in each LSOA and the average CO2 per km of cars in that LSOA. Mileage data from the 2011 MOTering Along project.</p>`;
     break;
+  case "gas_emissions_grade":
+    document.getElementById("layerdesc").innerHTML = `<p>Emissions from domestic gas consuption. THis is mostly due to domestic gas boilers for heating and hot water.</p>`;
+    break;
+  case "elec_emissions_grade":
+    document.getElementById("layerdesc").innerHTML = `<p>Emissions from domestic electricity consuption.</p>`;
+    break;
+  case "cars_percap_grade":
+    document.getElementById("layerdesc").innerHTML = `<p>The numer of cars per person. Higher car ownnership is known to lead to increased driving, and thus increased emissions.</p>`;
+    break;
   default:
     document.getElementById("layerdesc").innerHTML = `<p>No Description</p>`;
   } 
