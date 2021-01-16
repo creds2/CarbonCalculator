83a7b3c4de17980a3d9fe820365e841a324b9c6c
Malcolm Morgan <mem48@users.noreply.github.com>
2021-01-16 11:35:28 +0000

    togle layers

--- www/js/map_carbon.js
+++ www/js/map_carbon.js
@@ -36,91 +36,15 @@ map.addSource('la', {
 	'maxzoom': 13
 });
 
-map.addLayer({
-    'id': 'la',
-    'type': 'line',
-    'source': 'la',
-    'source-layer': 'la',
-    'paint': {
-      'line-color': 'rgba(0, 0, 0, 1)',
-      'line-width': 2
-    }
-});  
-
 map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+
+toggleLayer('carbon');
+toggleLayer('la');
 
 });
 
 
-function switchLayer(layer) {
-  var layerId = document.getElementById("layerinput").value;
-  var layers = map.getStyle().layers;
-  
-  if (map.getLayer('carbon')) map.removeLayer('carbon');
-  
-  switchLayerDesc();
-  
-  document.getElementById("legend").innerHTML = `<h4>Grades</h4>
-      <div><span style="background-color: #313695"></span>A+ (best 1%)</div>
-  		<div><span style="background-color: #4575b4"></span>A</div>
-  		<div><span style="background-color: #4575b4"></span>A- (best 10%)</div>
-  		<div><span style="background-color: #74add1"></span>B+</div>
-  		<div><span style="background-color: #abd9e9"></span>B</div>
-  		<div><span style="background-color: #abd9e9"></span>B-</div>
-  		<div><span style="background-color: #e0f3f8"></span>C+</div>
-  		<div><span style="background-color: #e0f3f8"></span>C</div>
-  		<div><span style="background-color: #ffffbf"></span>C- (just above average)</div>
-  		<div><span style="background-color: #ffffbf"></span>D+ (just below average)</div>
-  		<div><span style="background-color: #fee090"></span>D</div>
-  		<div><span style="background-color: #fee090"></span>D-</div>
-  		<div><span style="background-color: #fdae61"></span>E+</div>
-  		<div><span style="background-color: #fdae61"></span>E</div>
-  		<div><span style="background-color: #f46d43"></span>E-</div>
-  		<div><span style="background-color: #d73027"></span>F+ (worst 10%)</div>
-  		<div><span style="background-color: #d73027"></span>F</div>
-  		<div><span style="background-color: #a50026"></span>F- (worst 1%)</div>
-  		<div><span style="background-color: #e0e0e0"></span>No Data</div>`;
-      
-      
-  
-  map.addLayer(
-  {
-  'id': 'carbon',
-  'type': 'fill',
-  'source': 'carbon',
-  'source-layer': 'carbon',
-  "paint": {
-          "fill-color": [
-  			'match',
-  			['get', layerId],
-  			'A+','#313695',
-  			'A','#4575b4',
-  			'A-','#4575b4',
-  			'B+','#74add1',
-  			'B','#abd9e9',
-  			'B-','#abd9e9',
-  			'C+','#e0f3f8',
-  			'C','#e0f3f8',
-  			'C-','#ffffbf',
-  			'D+','#ffffbf',
-  			'D','#fee090',
-  			'D-','#fee090',
-  			'E+','#fdae61',
-  			'E','#fdae61',
-  			'E-','#f46d43',
-  			'F+','#d73027',
-  			'F','#d73027',
-  			'F-','#a50026',
-  			/* other */ '#e0e0e0'
-  			],
-          "fill-opacity": 0.7,
-          'fill-outline-color': 'rgba(0, 0, 0, 0.5)'
-        }
-  },  'landcover_grass' /*'housenumber'*/
-  );
-  
 
-}
 
 // On click open modal
 map.on('click', 'carbon', function(e) {
