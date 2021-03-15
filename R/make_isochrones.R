4f7ae0db6aa058e6bb1609435b9616ca24ceac41
Malcolm Morgan <mem48@users.noreply.github.com>
2021-03-15 18:30:02 +0000

    redo isochrones

--- R/make_isochrones.R
+++ R/make_isochrones.R
@@ -1,7 +1,7 @@
 library(opentripplanner)
 library(tmap)
 tmap_mode("view")
-otpcon <- otp_connect(router = "great-britain")
+otpcon <- otp_connect(router = "great-britain2")
 
 lsoa <- read_sf("data-prepared/lsoa_centroids.geojson")
 
@@ -11,9 +11,9 @@ iso_walk <- otp_isochrone(otpcon,
                           fromID = lsoa$code, 
                           mode = "WALK",
                           cutoffSec = 15*60,
-                          ncores = 20)
+                          ncores = 30)
 
-saveRDS(iso_walk, "data-prepared/isochones_walk.Rds")
+saveRDS(iso_walk, "data-prepared/isochones_walk_v2.Rds")
 
 
 iso_bike <- otp_isochrone(otpcon, 
@@ -21,9 +21,9 @@ iso_bike <- otp_isochrone(otpcon,
                           fromID = lsoa$code, 
                           mode = "BICYCLE",
                           cutoffSec = 15*60,
-                          ncores = 20)
+                          ncores = 30)
 
-saveRDS(iso_bike, "data-prepared/isochones_bike.Rds")
+saveRDS(iso_bike, "data-prepared/isochones_bike_v2.Rds")
 
 
 iso_transit <- otp_isochrone(otpcon, 
@@ -31,10 +31,10 @@ iso_transit <- otp_isochrone(otpcon,
                           fromID = lsoa$code, 
                           mode = c("WALK","TRANSIT"),
                           cutoffSec = 15*60,
-                          ncores = 20,
+                          ncores = 30,
                           date_time = lubridate::ymd_hms("2020-06-18 08:30:00"))
 
-saveRDS(iso_transit, "data-prepared/isochones_transit.Rds")
+saveRDS(iso_transit, "data-prepared/isochones_transit_v2.Rds")
 
 
 iso_biketransit <- otp_isochrone(otpcon, 
@@ -46,4 +46,4 @@ iso_biketransit <- otp_isochrone(otpcon,
                              date_time = lubridate::ymd_hms("2020-06-18 08:30:00"),
                              maxWalkDistance = 3000)
 
-saveRDS(iso_biketransit, "data-prepared/isochones_biketransit.Rds")
+saveRDS(iso_biketransit, "data-prepared/isochones_biketransit_v2.Rds")
