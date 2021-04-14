4bcf144100b1fbd0fe1c4cf8b004042bf316662a
Malcolm Morgan <mem48@users.noreply.github.com>
2021-04-14 17:25:11 +0100

    new cookies warning

--- www/js/ga_optout.js
+++ www/js/ga_optout.js
@@ -12,3 +12,58 @@ function gaOptout() {
   document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/; SameSite=None; Secure';
   window[disableStr] = true;
 }
+
+// Warning Control
+function cookiebutton(x) {
+  if(x === true){
+    // Cookies approved
+    setCookie('true');
+  } else if (x === false){
+    // Cookies rejected
+    alert("Tracking Op-Out Disabled");
+    //gaOptout();
+    //setCookie('false');
+  }
+  
+  var cookiewarning = document.getElementById("cookiewarning");
+  cookiewarning.style.display = "none";
+}
+
+
+// Cookie managment functions
+function setCookie(cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + (1000 * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = "PBCCtrack=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie() {
+  var name = "PBCCtrack=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function showCookieWarning(){
+  var cookiewarning = document.getElementById("cookiewarning");
+  var PBCCcookie = getCookie();
+  if (PBCCcookie === "") {
+    cookiewarning.style.display = "block";
+  } else {
+    cookiewarning.style.display = "none";
+  }
+}
+
+showCookieWarning();
+
+
+
