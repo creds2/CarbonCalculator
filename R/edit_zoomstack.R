86e0ec742a0b24276cf7cd4453d567bb4fb291b0
Malcolm Morgan <mem48@users.noreply.github.com>
2022-09-06 16:18:05 +0100

    new base map with prototiles

--- R/edit_zoomstack.R
+++ R/edit_zoomstack.R
@@ -2,93 +2,151 @@
 library(sf)
 library(tmap)
 tmap_mode("view")
+sf_use_s2(FALSE)
 
 # read in files
 
-land <- read_sf("data/zoomstackgeojson/land.geojson")
-names <- read_sf("data/zoomstackgeojson/names.geojson")
-greenspace <- read_sf("data/zoomstackgeojson/greenspace.geojson")
-airports <- read_sf("data/zoomstackgeojson/airports.geojson")
-boundaries <- read_sf("data/zoomstackgeojson/boundaries.geojson")
-foreshore <- read_sf("data/zoomstackgeojson/foreshore.geojson")
-national_parks <- read_sf("data/zoomstackgeojson/national_parks.geojson")
-rail <- read_sf("data/zoomstackgeojson/rail.geojson")
-railway_stations <- read_sf("data/zoomstackgeojson/railway_stations.geojson")
-roads_local <- read_sf("data/zoomstackgeojson/roads_local.geojson")
-roads_national <- read_sf("data/zoomstackgeojson/roads_national.geojson")
-roads_regional <- read_sf("data/zoomstackgeojson/roads_regional.geojson")
-surfacewater <- read_sf("data/zoomstackgeojson/surfacewater.geojson")
-woodland <- read_sf("data/zoomstackgeojson/woodland.geojson")
-urban_areas <- read_sf("data/zoomstackgeojson/urban_areas.geojson")
+land <- geojsonsf::geojson_sf("data/zoomstackgeojson/land.geojson")
 
 # Europe Boarders
-dir.create("tmp")
-unzip("data/europe bounds/countries_shp.zip", exdir = "tmp")
-bounds <- read_sf("tmp/countries.shp")
-unlink("tmp", recursive = TRUE)
+if(FALSE){
+  dir.create("tmp")
+  unzip("data/europe bounds/countries_shp.zip", exdir = "tmp")
+  bounds <- read_sf("tmp/countries.shp")
+  unlink("tmp", recursive = TRUE)
+  
+  box = c(xmin = -13, ymin = 47, xmax = 10, ymax = 63)
+  bounds <- st_crop(bounds, box)
+  bounds_uk <- bounds[bounds$NAME == "United Kingdom",]
+  bounds_eu <- bounds[bounds$NAME != "United Kingdom",]
+  bounds_uk <- st_cast(bounds_uk, to = "POLYGON", do_split = TRUE, group_or_split = TRUE)
+  bounds_uk$id <- 1:nrow(bounds_uk)
+  bounds_uk <- bounds_uk[bounds_uk$id %in% c(246,251, 207:212),]
+  bounds_uk$id <- NULL
+  
+  bounds_eu <- st_cast(bounds_eu$geometry, to = "MULTIPOLYGON", do_split = TRUE, group_or_split = TRUE)
+  bounds_eu <- st_cast(bounds_eu, to = "POLYGON", do_split = TRUE, group_or_split = TRUE)
+  
+  bounds_eu <- c(bounds_eu, bounds_uk$geometry)
+  bounds_eu <- st_union(bounds_eu)
+  
+  land <- st_transform(land, 27700)
+  land <- st_buffer(st_combine(land), 0)
+  land <- st_transform(land, 4326)
+  land <- st_cast(land, to = "POLYGON", do_split = TRUE, group_or_split = TRUE)
+  
+  land_all <- c(land, bounds_eu)
+  land_all <- st_combine(land_all)
+  land_all <- st_make_valid(land_all)
+  
+  sea <- st_bbox(box)
+  sea <- st_as_sfc(sea)
+  st_crs(sea) <- 4326
+  
+  sea <- st_difference(sea, land_all)
+  sea <- st_collection_extract(sea, "POLYGON")
+  sea <- st_cast(sea, "POLYGON")
+  sea <- st_sf(data.frame(geom = sea))
+  sea$id <- 1:nrow(sea)
+  sea$area <- as.numeric(st_area(sea))
+  hist(sea$area)
+  sea <- sea$geometry[sea$area == max(sea$area)]
+  
+  st_write(sea, "data/zoomstackgeojson/sea.geojson", delete_dsn = TRUE)
+  st_write(land_all, "data/zoomstackgeojson/land_uk_eu.geojson", delete_dsn = TRUE)
+} else{
+  sea <- geojsonsf::geojson_sf("data/zoomstackgeojson/sea.geojson")
+  land_all <- geojsonsf::geojson_sf("data/zoomstackgeojson/land_uk_eu.geojson")
+}
 
-box = c(xmin = -13, ymin = 47, xmax = 10, ymax = 63)
-bounds <- st_crop(bounds, box)
-bounds_uk <- bounds[bounds$NAME == "United Kingdom",]
-bounds_eu <- bounds[bounds$NAME != "United Kingdom",]
-bounds_uk <- st_cast(bounds_uk, to = "POLYGON", do_split = TRUE, group_or_split = TRUE)
-bounds_uk$id <- 1:nrow(bounds_uk)
-bounds_uk <- bounds_uk[bounds_uk$id %in% c(246,251, 207:212),]
-bounds_uk$id <- NULL
 
-bounds_eu <- rbind(bounds_eu, bounds_uk)
-bounds_eu <- bounds_eu$geometry
-bounds_eu <- st_union(bounds_eu)
+# Make a close sea
+land_simple <- st_transform(land, 27700)
+land_simple <- st_simplify(land_simple, 1000, preserveTopology = TRUE)
+land_buff <- st_buffer(land_simple, 30000, nQuadSegs = 1, endCapStyle = "SQUARE")
+land_buff <- st_union(land_buff)
+land_buff <- st_transform(land_buff, 4326)
+sea_near <- st_difference(land_buff, land_all)
+sea_near <- st_cast(sea_near, "POLYGON")
 
-land_all <- c(land$geometry, bounds_eu)
-land_all <- st_union(land_all)
+land_buff <- st_buffer(land_simple, 100000, nQuadSegs = 1, endCapStyle = "SQUARE")
+land_buff <- st_union(land_buff)
+land_buff <- st_transform(land_buff, 4326)
+sea_medium <- st_difference(land_buff, land_all)
+sea_medium <- st_cast(sea_medium, "POLYGON")
 
-sea <- st_bbox(box)
-sea <- st_as_sfc(sea)
-st_crs(sea) <- 4326
-sea <- st_difference(sea, land_all)
-sea <- st_cast(sea, "POLYGON")
-sea <- st_sf(data.frame(geom = sea))
-sea$id <- 1:9
-sea <- sea$geometry[1]
-st_write(sea, "data/zoomstackgeojson/sea.geojson")
+land_buff <- st_buffer(land_simple, 10000, nQuadSegs = 1, endCapStyle = "SQUARE")
+land_buff <- st_union(land_buff)
+land_buff <- st_transform(land_buff, 4326)
+sea_verynear <- st_difference(land_buff, land_all)
+sea_verynear <- st_cast(sea_verynear, "POLYGON")
 
+#land_buff <- st_buffer(land_simple, 2000, nQuadSegs = 1, endCapStyle = "SQUARE")
+#land_buff <- st_union(land_buff)
+#land_buff <- st_transform(land_buff, 4326)
+#sea_ultranear <- st_difference(land_buff, land_all)
+#sea_ultranear <- st_cast(sea_verynear, "POLYGON")
 
+sea_near <- sea_near[lengths(sea_near) != 1]
+sea_medium <- sea_medium[lengths(sea_medium) != 1]
+sea_verynear <- sea_verynear[lengths(sea_verynear) != 1]
+#sea_ultranear <- sea_ultranear[lengths(sea_ultranear) != 1]
 
+# plot(sea)
+# plot(sea_medium, add = TRUE, border = 'red')
+# plot(sea_near, add = TRUE, border = 'green')
+# plot(sea_verynear, add = TRUE, border = 'blue')
+# plot(sea_ultranear, add = TRUE, border = 'black')
+
+# Other layers
+name <- geojsonsf::geojson_sf("data/zoomstackgeojson/names.geojson")
+greenspace <- geojsonsf::geojson_sf("data/zoomstackgeojson/greenspace.geojson")
+airports <- read_sf("data/zoomstackgeojson/airports.geojson")
+sites <- geojsonsf::geojson_sf("data/zoomstackgeojson/sites.geojson")
+boundaries <- geojsonsf::geojson_sf("data/zoomstackgeojson/boundaries.geojson")
+foreshore <- geojsonsf::geojson_sf("data/zoomstackgeojson/foreshore.geojson")
+national_parks <- geojsonsf::geojson_sf("data/zoomstackgeojson/national_parks.geojson")
+rail <- geojsonsf::geojson_sf("data/zoomstackgeojson/rail.geojson")
+railway_stations <- geojsonsf::geojson_sf("data/zoomstackgeojson/railway_stations.geojson")
+roads_local <- geojsonsf::geojson_sf("data/zoomstackgeojson/roads_local.geojson")
+roads_national <- geojsonsf::geojson_sf("data/zoomstackgeojson/roads_national.geojson")
+roads_regional <- geojsonsf::geojson_sf("data/zoomstackgeojson/roads_regional.geojson")
+surfacewater <- geojsonsf::geojson_sf("data/zoomstackgeojson/surfacewater.geojson")
+woodland <- geojsonsf::geojson_sf("data/zoomstackgeojson/woodland.geojson")
+urban_areas <- geojsonsf::geojson_sf("data/zoomstackgeojson/urban_areas.geojson")
+district_buildings <- geojsonsf::geojson_sf("data/zoomstackgeojson/district_buildings.geojson")
+local_buildings <- geojsonsf::geojson_sf("data/zoomstackgeojson/local_buildings.geojson")
+
+name <- name[,c("type","name1")]
+names(name) <- c("type","name","geometry")
 greenspace$area <- as.numeric(st_area(greenspace))
 foreshore$area <- as.numeric(st_area(foreshore))
 surfacewater$area <- as.numeric(st_area(surfacewater))
 woodland$area <- as.numeric(st_area(woodland))
-urban_areas$area <- as.numeric(st_area(urban_areas))
-
 
 # Low Zoom
-names_low <- names[names$type %in% c("Capital","City","Country","Town"),]
+names_low <- name[name$type %in% c("Capital","City","Country","Town"),]
 greenspace_low <- greenspace[greenspace$area > 100000,]
 foreshore_low <- foreshore[foreshore$area > 100000,]
 surfacewater_low <- surfacewater[surfacewater$area > 1000000,]
 woodland_low <- woodland[woodland$area > 1000000,]
-urban_areas_low <- urban_areas[urban_areas$area > 100000,]
 
 greenspace_low$area <- NULL
 foreshore_low$area <- NULL
 surfacewater_low$area <- NULL
 woodland_low$area <- NULL
-urban_areas_low$area <- NULL
 
 # Med zoom
-names_med <- names[names$type %in% c("Capital","City","Country","National Park","Town","Suburban Area"),]
+names_med <- name[name$type %in% c("Capital","City","Country","National Park","Town","Suburban Area"),]
 greenspace_med <- greenspace[greenspace$area > 10000,]
 foreshore_med <- foreshore[foreshore$area > 10000,]
 surfacewater_med <- surfacewater[surfacewater$area > 10000,]
 woodland_med <- woodland[woodland$area > 10000,]
-urban_areas_med <- urban_areas[urban_areas$area > 10000,]
 
 greenspace_med$area <- NULL
 foreshore_med$area <- NULL
 surfacewater_med$area <- NULL
 woodland_med$area <- NULL
-urban_areas_med$area <- NULL
 
 greenspace$area <- NULL
 foreshore$area <- NULL
@@ -100,47 +158,74 @@ urban_areas$area <- NULL
 roads_regional <- rbind(roads_national, roads_regional)
 roads_local <- rbind(roads_regional, roads_local)
 
-# Low Zoom
-write_sf(land, "data/zoomstackgeojson/low/land.geojson", delete_dsn = TRUE)
-write_sf(names_low, "data/zoomstackgeojson/low/names.geojson", delete_dsn = TRUE)
-write_sf(greenspace_low, "data/zoomstackgeojson/low/greenspace.geojson", delete_dsn = TRUE)
-write_sf(boundaries, "data/zoomstackgeojson/low/boundaries.geojson", delete_dsn = TRUE)
-write_sf(foreshore_low, "data/zoomstackgeojson/low/foreshore.geojson", delete_dsn = TRUE)
-write_sf(national_parks, "data/zoomstackgeojson/low/national_parks.geojson", delete_dsn = TRUE)
-write_sf(rail[1,], "data/zoomstackgeojson/low/rail.geojson", delete_dsn = TRUE)
-write_sf(railway_stations[1,], "data/zoomstackgeojson/low/railway_stations.geojson", delete_dsn = TRUE)
-write_sf(roads_national, "data/zoomstackgeojson/low/roads.geojson", delete_dsn = TRUE)
-write_sf(surfacewater_low, "data/zoomstackgeojson/low/surfacewater.geojson", delete_dsn = TRUE)
-write_sf(woodland_low, "data/zoomstackgeojson/low/woodland.geojson", delete_dsn = TRUE)
-write_sf(urban_areas_low, "data/zoomstackgeojson/low/urban_areas.geojson", delete_dsn = TRUE)
-write_sf(sea, "data/zoomstackgeojson/low/sea.geojson", delete_dsn = TRUE)
+# Low Zoom 0 - 8
+write_sf(names_low, "C:/tiles/oszoom/low/names.geojson", delete_dsn = TRUE)
+write_sf(greenspace_low, "C:/tiles/oszoom/low/greenspace.geojson", delete_dsn = TRUE)
+write_sf(boundaries, "C:/tiles/oszoom/low/boundaries.geojson", delete_dsn = TRUE)
+write_sf(foreshore_low, "C:/tiles/oszoom/low/foreshore.geojson", delete_dsn = TRUE)
+write_sf(national_parks, "C:/tiles/oszoom/low/national_parks.geojson", delete_dsn = TRUE)
+write_sf(roads_national, "C:/tiles/oszoom/low/roads.geojson", delete_dsn = TRUE)
+write_sf(surfacewater_low, "C:/tiles/oszoom/low/surfacewater.geojson", delete_dsn = TRUE)
+write_sf(woodland_low, "C:/tiles/oszoom/low/woodland.geojson", delete_dsn = TRUE)
+write_sf(urban_areas, "C:/tiles/oszoom/low/urban_areas.geojson", delete_dsn = TRUE)
+write_sf(sea, "C:/tiles/oszoom/low/sea.geojson", delete_dsn = TRUE)
 
-#Med Zoom
-write_sf(land, "data/zoomstackgeojson/med/land.geojson", delete_dsn = TRUE)
-write_sf(names_med, "data/zoomstackgeojson/med/names.geojson", delete_dsn = TRUE)
-write_sf(greenspace_med, "data/zoomstackgeojson/med/greenspace.geojson", delete_dsn = TRUE)
-write_sf(boundaries, "data/zoomstackgeojson/med/boundaries.geojson", delete_dsn = TRUE)
-write_sf(foreshore_med, "data/zoomstackgeojson/med/foreshore.geojson", delete_dsn = TRUE)
-write_sf(national_parks, "data/zoomstackgeojson/med/national_parks.geojson", delete_dsn = TRUE)
-write_sf(rail, "data/zoomstackgeojson/med/rail.geojson", delete_dsn = TRUE)
-write_sf(railway_stations[1,], "data/zoomstackgeojson/med/railway_stations.geojson", delete_dsn = TRUE)
-write_sf(roads_regional, "data/zoomstackgeojson/med/roads.geojson", delete_dsn = TRUE)
-write_sf(surfacewater_med, "data/zoomstackgeojson/med/surfacewater.geojson", delete_dsn = TRUE)
-write_sf(woodland_med, "data/zoomstackgeojson/med/woodland.geojson", delete_dsn = TRUE)
-write_sf(urban_areas_med, "data/zoomstackgeojson/med/urban_areas.geojson", delete_dsn = TRUE)
-write_sf(sea, "data/zoomstackgeojson/med/sea.geojson", delete_dsn = TRUE)
+#Med Zoom 9 - 11
+write_sf(names_med, "C:/tiles/oszoom/med/names.geojson", delete_dsn = TRUE)
+write_sf(greenspace_med, "C:/tiles/oszoom/med/greenspace.geojson", delete_dsn = TRUE)
+write_sf(boundaries, "C:/tiles/oszoom/med/boundaries.geojson", delete_dsn = TRUE)
+write_sf(foreshore_med, "C:/tiles/oszoom/med/foreshore.geojson", delete_dsn = TRUE)
+write_sf(national_parks, "C:/tiles/oszoom/med/national_parks.geojson", delete_dsn = TRUE)
+write_sf(rail, "C:/tiles/oszoom/high/rail.geojson", delete_dsn = TRUE)
+write_sf(roads_regional, "C:/tiles/oszoom/med/roads.geojson", delete_dsn = TRUE)
+write_sf(surfacewater_med, "C:/tiles/oszoom/med/surfacewater.geojson", delete_dsn = TRUE)
+write_sf(woodland_med, "C:/tiles/oszoom/med/woodland.geojson", delete_dsn = TRUE)
+write_sf(urban_areas, "C:/tiles/oszoom/med/urban_areas.geojson", delete_dsn = TRUE)
+write_sf(airports, "C:/tiles/oszoom/med/airports.geojson", delete_dsn = TRUE)
+write_sf(sea_medium, "C:/tiles/oszoom/med/sea.geojson", delete_dsn = TRUE)
 
-#high Zoom
-write_sf(land, "data/zoomstackgeojson/high/land.geojson", delete_dsn = TRUE)
-write_sf(names, "data/zoomstackgeojson/high/names.geojson", delete_dsn = TRUE)
-write_sf(greenspace, "data/zoomstackgeojson/high/greenspace.geojson", delete_dsn = TRUE)
-write_sf(boundaries, "data/zoomstackgeojson/high/boundaries.geojson", delete_dsn = TRUE)
-write_sf(foreshore, "data/zoomstackgeojson/high/foreshore.geojson", delete_dsn = TRUE)
-write_sf(national_parks, "data/zoomstackgeojson/high/national_parks.geojson", delete_dsn = TRUE)
-write_sf(rail, "data/zoomstackgeojson/high/rail.geojson", delete_dsn = TRUE)
-write_sf(railway_stations, "data/zoomstackgeojson/high/railway_stations.geojson", delete_dsn = TRUE)
-write_sf(roads_local, "data/zoomstackgeojson/high/roads.geojson", delete_dsn = TRUE)
-write_sf(surfacewater, "data/zoomstackgeojson/high/surfacewater.geojson", delete_dsn = TRUE)
-write_sf(woodland, "data/zoomstackgeojson/high/woodland.geojson", delete_dsn = TRUE)
-write_sf(urban_areas, "data/zoomstackgeojson/high/urban_areas.geojson", delete_dsn = TRUE)
-write_sf(sea, "data/zoomstackgeojson/high/sea.geojson", delete_dsn = TRUE)
+#high Zoom 12
+write_sf(name, "C:/tiles/oszoom/high/names.geojson", delete_dsn = TRUE)
+write_sf(greenspace, "C:/tiles/oszoom/high/greenspace.geojson", delete_dsn = TRUE)
+write_sf(boundaries, "C:/tiles/oszoom/high/boundaries.geojson", delete_dsn = TRUE)
+write_sf(foreshore, "C:/tiles/oszoom/high/foreshore.geojson", delete_dsn = TRUE)
+write_sf(rail, "C:/tiles/oszoom/high/rail.geojson", delete_dsn = TRUE)
+write_sf(railway_stations, "C:/tiles/oszoom/high/railway_stations.geojson", delete_dsn = TRUE)
+write_sf(roads_local, "C:/tiles/oszoom/high/roads.geojson", delete_dsn = TRUE)
+write_sf(surfacewater, "C:/tiles/oszoom/high/surfacewater.geojson", delete_dsn = TRUE)
+write_sf(woodland, "C:/tiles/oszoom/high/woodland.geojson", delete_dsn = TRUE)
+write_sf(urban_areas, "C:/tiles/oszoom/high/urban_areas.geojson", delete_dsn = TRUE)
+write_sf(sites, "C:/tiles/oszoom/high/sites.geojson", delete_dsn = TRUE)
+write_sf(airports, "C:/tiles/oszoom/high/airports.geojson", delete_dsn = TRUE)
+write_sf(sea_near, "C:/tiles/oszoom/high/sea.geojson", delete_dsn = TRUE)
+
+#veryhigh Zoom 13
+write_sf(name, "C:/tiles/oszoom/veryhigh/names.geojson", delete_dsn = TRUE)
+write_sf(greenspace, "C:/tiles/oszoom/veryhigh/greenspace.geojson", delete_dsn = TRUE)
+write_sf(boundaries, "C:/tiles/oszoom/veryhigh/boundaries.geojson", delete_dsn = TRUE)
+write_sf(foreshore, "C:/tiles/oszoom/veryhigh/foreshore.geojson", delete_dsn = TRUE)
+write_sf(rail, "C:/tiles/oszoom/veryhigh/rail.geojson", delete_dsn = TRUE)
+write_sf(railway_stations, "C:/tiles/oszoom/veryhigh/railway_stations.geojson", delete_dsn = TRUE)
+write_sf(roads_local, "C:/tiles/oszoom/veryhigh/roads.geojson", delete_dsn = TRUE)
+write_sf(surfacewater, "C:/tiles/oszoom/veryhigh/surfacewater.geojson", delete_dsn = TRUE)
+write_sf(woodland, "C:/tiles/oszoom/veryhigh/woodland.geojson", delete_dsn = TRUE)
+write_sf(sites, "C:/tiles/oszoom/veryhigh/sites.geojson", delete_dsn = TRUE)
+write_sf(airports, "C:/tiles/oszoom/veryhigh/airports.geojson", delete_dsn = TRUE)
+write_sf(sea_verynear, "C:/tiles/oszoom/veryhigh/sea.geojson", delete_dsn = TRUE)
+write_sf(district_buildings, "C:/tiles/oszoom/veryhigh/buildings.geojson", delete_dsn = TRUE)
+
+#ultrahigh Zoom 14
+write_sf(name, "C:/tiles/oszoom/ultrahigh/names.geojson", delete_dsn = TRUE)
+write_sf(greenspace, "C:/tiles/oszoom/ultrahigh/greenspace.geojson", delete_dsn = TRUE)
+write_sf(boundaries, "C:/tiles/oszoom/ultrahigh/boundaries.geojson", delete_dsn = TRUE)
+write_sf(foreshore, "C:/tiles/oszoom/ultrahigh/foreshore.geojson", delete_dsn = TRUE)
+write_sf(rail, "C:/tiles/oszoom/ultrahigh/rail.geojson", delete_dsn = TRUE)
+write_sf(railway_stations, "C:/tiles/oszoom/ultrahigh/railway_stations.geojson", delete_dsn = TRUE)
+write_sf(roads_local, "C:/tiles/oszoom/ultrahigh/roads.geojson", delete_dsn = TRUE)
+write_sf(surfacewater, "C:/tiles/oszoom/ultrahigh/surfacewater.geojson", delete_dsn = TRUE)
+write_sf(woodland, "C:/tiles/oszoom/ultrahigh/woodland.geojson", delete_dsn = TRUE)
+write_sf(sites, "C:/tiles/oszoom/ultrahigh/sites.geojson", delete_dsn = TRUE)
+write_sf(airports, "C:/tiles/oszoom/ultrahigh/airports.geojson", delete_dsn = TRUE)
+write_sf(sea_verynear, "C:/tiles/oszoom/ultrahigh/sea.geojson", delete_dsn = TRUE)
+write_sf(local_buildings, "C:/tiles/oszoom/ultrahigh/buildings.geojson", delete_dsn = TRUE)
+
--- make_os_zoomstack.R
+++ make_os_zoomstack.R
@@ -1,101 +1,109 @@
 # read in layers
+
 library(sf)
 
-land <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                layer = "land")
-land <- st_transform(land, 4326)
-write_sf(land, "data/zoomstackgeojson/land.geojson", delete_dsn = TRUE)
+path = "D:/OneDrive - University of Leeds/Data/OS/ZoomStack/2022/OS_Open_Zoomstack/OS_Open_Zoomstack.gpkg"
 
-name <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                layer = "names")
+name <- read_sf(path,layer = "names")
 name <- name[!name$type %in% c("Sites","Greenspace","Motorway Junction",
                                "Woodland", "Small Settlements","Water"),]
 name <- st_transform(name, 4326)
+st_precision(name) <- 1000000 
 write_sf(name, "data/zoomstackgeojson/names.geojson", delete_dsn = TRUE)
 
-greenspace <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                layer = "greenspace")
+land <- read_sf(path,layer = "land")
+land <- st_transform(land, 4326)
+st_precision(land) <- 1000000 # About 3 cm precision
+write_sf(land, "data/zoomstackgeojson/land.geojson", delete_dsn = TRUE)
+
+greenspace <- read_sf(path,layer = "greenspace")
 greenspace <- st_transform(greenspace, 4326)
+st_precision(greenspace) <- 1000000
 write_sf(greenspace, "data/zoomstackgeojson/greenspace.geojson", delete_dsn = TRUE)
 
-airports <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                      layer = "airports")
+airports <- read_sf(path,layer = "airports")
 airports <- st_transform(airports, 4326)
+st_precision(airports) <- 1000000
 write_sf(airports, "data/zoomstackgeojson/airports.geojson", delete_dsn = TRUE)
 
-boundaries <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                    layer = "boundaries")
+boundaries <- read_sf(path,layer = "boundaries")
 boundaries <- st_transform(boundaries, 4326)
+st_precision(boundaries) <- 1000000
 write_sf(boundaries, "data/zoomstackgeojson/boundaries.geojson", delete_dsn = TRUE)
 
-contours <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                      layer = "contours")
-contours <- st_transform(contours, 4326)
-write_sf(contours, "data/zoomstackgeojson/contours.geojson", delete_dsn = TRUE)
+# contours <- read_sf(path,
+#                       layer = "contours")
+# contours <- st_transform(contours, 4326)
+# st_precision(contours) <- 1000000
+# write_sf(contours, "data/zoomstackgeojson/contours.geojson", delete_dsn = TRUE)
 
-district_buildings <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                    layer = "district_buildings")
+district_buildings <- read_sf(path,layer = "district_buildings")
 district_buildings <- st_transform(district_buildings, 4326)
+st_precision(district_buildings) <- 1000000
 write_sf(district_buildings, "data/zoomstackgeojson/district_buildings.geojson", delete_dsn = TRUE)
 
-# etl <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-#                               layer = "etl")
-# etl <- st_transform(etl, 4326)
-# write_sf(etl, "data/zoomstackgeojson/etl.geojson", delete_dsn = TRUE)
+local_buildings <- read_sf(path,layer = "local_buildings")
+local_buildings <- st_transform(local_buildings, 4326)
+local_buildings$uuid <- NULL
+st_precision(local_buildings) <- 1000000
+write_sf(local_buildings, "data/zoomstackgeojson/local_buildings.geojson", delete_dsn = TRUE)
 
+etl <- read_sf(path,layer = "etl")
+etl <- st_transform(etl, 4326)
+st_precision(etl) <- 1000000
+write_sf(etl, "data/zoomstackgeojson/etl.geojson", delete_dsn = TRUE)
 
-foreshore <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-               layer = "foreshore")
+foreshore <- read_sf(path,layer = "foreshore")
 foreshore <- st_transform(foreshore, 4326)
+st_precision(foreshore) <- 1000000
 write_sf(foreshore, "data/zoomstackgeojson/foreshore.geojson", delete_dsn = TRUE)
 
-national_parks <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                     layer = "national_parks")
+national_parks <- read_sf(path,layer = "national_parks")
 national_parks <- st_transform(national_parks, 4326)
+st_precision(national_parks) <- 1000000
 write_sf(national_parks, "data/zoomstackgeojson/national_parks.geojson", delete_dsn = TRUE)
 
-rail <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                          layer = "rail")
+rail <- read_sf(path,layer = "rail")
 rail <- st_transform(rail, 4326)
+st_precision(rail) <- 1000000
 write_sf(rail, "data/zoomstackgeojson/rail.geojson", delete_dsn = TRUE)
 
-railway_stations <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                layer = "railway_stations")
+railway_stations <- read_sf(path,layer = "railway_stations")
 railway_stations <- st_transform(railway_stations, 4326)
+st_precision(railway_stations) <- 1000000
 write_sf(railway_stations, "data/zoomstackgeojson/railway_stations.geojson", delete_dsn = TRUE)
 
-roads_local <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                            layer = "roads_local")
+roads_local <- read_sf(path,layer = "roads_local")
 roads_local <- st_transform(roads_local, 4326)
+st_precision(roads_local) <- 1000000
 write_sf(roads_local, "data/zoomstackgeojson/roads_local.geojson", delete_dsn = TRUE)
 
-
-roads_national <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                       layer = "roads_national")
+roads_national <- read_sf(path,layer = "roads_national")
 roads_national <- st_transform(roads_national, 4326)
+st_precision(roads_national) <- 1000000
 write_sf(roads_national, "data/zoomstackgeojson/roads_national.geojson", delete_dsn = TRUE)
 
-roads_regional <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                          layer = "roads_regional")
+roads_regional <- read_sf(path,layer = "roads_regional")
 roads_regional <- st_transform(roads_regional, 4326)
+st_precision(roads_regional) <- 1000000
 write_sf(roads_regional, "data/zoomstackgeojson/roads_regional.geojson", delete_dsn = TRUE)
 
-sites <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                          layer = "sites")
+sites <- read_sf(path,layer = "sites")
 sites <- st_transform(sites, 4326)
+st_precision(sites) <- 1000000
 write_sf(sites, "data/zoomstackgeojson/sites.geojson", delete_dsn = TRUE)
 
-surfacewater <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                 layer = "surfacewater")
+surfacewater <- read_sf(path,layer = "surfacewater")
 surfacewater <- st_transform(surfacewater, 4326)
+st_precision(surfacewater) <- 1000000
 write_sf(surfacewater, "data/zoomstackgeojson/surfacewater.geojson", delete_dsn = TRUE)
 
-urban_areas <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                        layer = "urban_areas")
+urban_areas <- read_sf(path,layer = "urban_areas")
 urban_areas <- st_transform(urban_areas, 4326)
+st_precision(urban_areas) <- 1000000
 write_sf(urban_areas, "data/zoomstackgeojson/urban_areas.geojson", delete_dsn = TRUE)
 
-woodland <- read_sf("D:/OneDrive - University of Leeds/Data/OS/ZoomStack/OSOpen_ZoomStack_GPKG/OSOpen_ZoomStack_v0_3.gpkg",
-                       layer = "woodland")
+woodland <- read_sf(path,layer = "woodland")
 woodland <- st_transform(woodland, 4326)
+st_precision(woodland) <- 1000000
 write_sf(woodland, "data/zoomstackgeojson/woodland.geojson", delete_dsn = TRUE)
